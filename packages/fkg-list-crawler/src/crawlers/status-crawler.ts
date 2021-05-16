da63f3bb8df37131c31ad5c20ed674d844d25e38
_MoonLight_ <moonlight8978@gmail.com>
2021-05-16 22:20:34 +0700

    Add log to debug downloading

--- packages/fkg-list-crawler/src/crawlers/status-crawler.ts
+++ packages/fkg-list-crawler/src/crawlers/status-crawler.ts
@@ -30,6 +30,9 @@ export class StatusCrawler implements Crawler<StatusCrawlerOutput, Cache> {
       return fs.readFileSync(pageFilePath).toString()
     }
 
+    console.log(
+      '--- StatusCrawler: Downloading https://xn--eckq7fg8cygsa1a1je.xn--wiki-4i9hs14f.com/index.php?%E8%8A%B1%E9%A8%8E%E5%A3%AB%20%E3%82%B9%E3%83%86%E3%83%BC%E3%82%BF%E3%82%B9%E4%B8%80%E8%A6%A7'
+    )
     const response = await axios.get(
       'https://xn--eckq7fg8cygsa1a1je.xn--wiki-4i9hs14f.com/index.php?%E8%8A%B1%E9%A8%8E%E5%A3%AB%20%E3%82%B9%E3%83%86%E3%83%BC%E3%82%BF%E3%82%B9%E4%B8%80%E8%A6%A7'
     )
