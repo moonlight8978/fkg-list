1ad1e4c1c49abab0eb077b9502d0340ce95ffa83
Lê Sĩ Bích <moonlight8978@gmail.com>
2021-05-20 17:03:01 +0700

    Release v2.3.0 (#37)

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,3 +1,4 @@
 for directory in . ./packages/fkg-list-crawler ./packages/fkg-list-shared ./packages/fkg-list-types ./packages/fkg-list-webapp; do
   yarn json --in-place -f "${directory}/package.json" -e "this.version = '$1'"
 done
+yarn json --in-place -f lerna.json -e "this.version = '$1'"
